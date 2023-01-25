64132f9126de2fc3b7ad08df7565966476a15413
unknown <eliasver99@gmail.com>
2023-01-25 16:13:34 +0200

    axios fix

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "https://www.itsallpolitics.gr/api/",
+  baseURL: "https://api.itsallpolitics.gr/api/",
   headers: {
     "Content-Type": "application/json",
     timeout: 0,
